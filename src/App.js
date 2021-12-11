a7a3526989705f3467dfe5c3ec2ee8bd20c801ea
adilsonxavier <adilsonxdesouza@gmail.com>
2021-12-11 16:45:17 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+
+export function App() {
+    return (
+        <div>
+            <h1>Adilson xavier</h1>
+            <p>{ new Date().toString()}</p>
+        </div>
+    );
+}
